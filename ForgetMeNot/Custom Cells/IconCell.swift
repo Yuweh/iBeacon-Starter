9f1faed28fb85919860b7a3794e84be99fb86cf0
Francis Jemuel Bergonia <francisb.itcareers@gmail.com>
2018-05-23 08:30:57 +0800

    Initial Phase

---
+++ ForgetMeNot/Custom Cells/IconCell.swift
@@ -0,0 +1,16 @@
+
+
+import UIKit
+
+class IconCell: UICollectionViewCell {
+    
+  @IBOutlet weak var imgIcon: UIImageView!
+  
+  var icon: Icons? {
+    didSet {
+      guard let icon = icon else { return }
+      imgIcon.image = icon.image()
+    }
+  }
+  
+}
